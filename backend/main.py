3bae8cb16a7645d9bcc99b4200af2748993cbddd
James Twose <jamestwose@Mac-mini.home>
2022-06-05 16:11:15 +0200

    attempting to sort out the frontend for the API

---
+++ backend/main.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI
+from deta import Deta
+# from dotenv import load_dotenv, find_dotenv
+import os
+# #%%
+# load_dotenv(find_dotenv())
+
+# #%%
+# # initialize with a project key
+# deta = Deta(os.environ["health_deta_project_key"])
+
+# #%%
+# # create the deta Base
+# health_db = deta.Base("health_deta_base")
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/all")
+def read_all():
+    return [{'date': '2020-12-31',
+  'key': 'z0zocui27zi6',
+  'mean_bpm': 89.29,
+  'mean_confidence': 1.86},
+ {'date': '2021-01-01',
+  'key': 'l9krrctsoxx8',
+  'mean_bpm': 76.68,
+  'mean_confidence': 2.08},
+ {'date': '2021-01-02',
+  'key': 'rkxjlznt1tk5',
+  'mean_bpm': 74.02,
+  'mean_confidence': 2.36},
+ {'date': '2021-01-03',
+  'key': 'pc2njrew5e98',
+  'mean_bpm': 81.56,
+  'mean_confidence': 2.24},
+ {'date': '2021-01-04',
+  'key': 'v1sfuj1qi3d5',
+  'mean_bpm': 77.08,
+  'mean_confidence': 2.16},
+ {'date': '2021-01-05',
+  'key': '5zlos8waqv63',
+  'mean_bpm': 71.87,
+  'mean_confidence': 2.33}]
---
+++ database/deta_database.py
@@ -0,0 +1,47 @@
+#%%
+# read in necessary functions
+from deta import Deta
+import numpy as np
+import pandas as pd
+from dotenv import load_dotenv, find_dotenv
+import os
+import seaborn as sns
+#%%
+load_dotenv(find_dotenv())
+
+#%%
+# initialize with a project key
+deta = Deta(os.environ["health_deta_project_key"])
+
+#%%
+# create the deta Base
+health_db = deta.Base("health_deta_base")
+
+# %%
+# Upload health data from an open repository
+_df = pd.read_csv("https://raw.githubusercontent.com/jameshtwose/jms_fitbit_analyses/eda/DS/MyFitbitData/JamesTwose/Physical%20Activity/heart_rate_per_hour.csv")
+df = (_df
+ .assign(**{"date": lambda d: pd.to_datetime(d["dateTime"]).dt.date.astype(str)})
+ .groupby("date")
+ .mean()
+ .round(2)
+ .reset_index()
+ .rename(columns={"bpm": "mean_bpm", "confidence": "mean_confidence"})
+ )
+# %%
+inserted_values = [health_db.insert(data=row_to_insert) for row_to_insert in df.to_dict(orient="records")]
+# inserted_values
+# %%
+# Fetch data and reassign data types/ drop default key column
+df = (pd.DataFrame(health_db.fetch().items)
+ .assign(**{"date": lambda d: pd.to_datetime(d["date"]).dt.date})
+ .drop("key", axis=1)
+ .sort_values(by="date")
+ .reset_index(drop=True)
+ )
+# %%
+plot_df = df.melt(id_vars="date")
+_ = sns.relplot(data=plot_df, x="date", y="value", hue="variable",
+                row="variable", facet_kws={"sharey":False},
+                kind="line", aspect=2.5)
+# %%
