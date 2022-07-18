a95206dff46bb367614d523d03e7f655b21f17ef
James Twose <jamestwose@Mac-mini.home>
2022-07-18 20:27:28 +0200

    getting the front end to work with the line plot - next step connect API to cockroachDB

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,5 @@
 // alert("hi");
 
-// import Chart from 'https://cdn.jsdelivr.net/npm/chart.js';
-
 // api url
 const api_url = "https://2q6e55.deta.dev/all";
 const options = {
@@ -27,12 +25,14 @@ fetch(api_url, options)
     displayDate(data)
     displayBpm(data)
     displayConfidence(data)
+    // displayAll(data)
+    displayPlot(data)
   })
   .catch((error) => console.error("FETCH ERROR:", error));
 
 function displayDate(data) {
 
-  const date = data[0];
+  const date = data[data.length - 1];
   const dateDiv = document.getElementById("date");    
 
   // date name
@@ -45,80 +45,93 @@ function displayDate(data) {
 
 function displayBpm(data) {
 
-  const bpm = data[0];
+  const bpm = data[data.length - 1];
   const bpmDiv = document.getElementById("bpm");    
 
   // bpm name
   const bpmBpm = bpm.mean_bpm;
   const heading = document.createElement("h2");
-  heading.innerHTML = "Average Bpm: " + bpmBpm;
+  heading.innerHTML = "Average Bpm On This Date: " + bpmBpm;
   bpmDiv.appendChild(heading);
 
 }
 
 function displayConfidence(data) {
 
-  const confidence = data[0];
+  const confidence = data[data.length - 1];
   const confidenceDiv = document.getElementById("confidence");    
 
   // confidence name
   const bpmConfidence = confidence.mean_confidence;
   const heading = document.createElement("h2");
-  heading.innerHTML = "Average Confidence: " + bpmConfidence;
+  heading.innerHTML = "Average Confidence On This Date: " + bpmConfidence;
   confidenceDiv.appendChild(heading);
 
 }
 
-// var ctx = document.getElementById("myChart");
-// var myChart = new Chart(ctx, {
-//   type: "line",
-//   data: {
-//     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-//     datasets: [
-//       {
-//         label: "# of Votes",
-//         data: [12, 19, 3, 5, 2, 3]
-//       }
-//     ]
-//   },
-//   options: {
-//     scales: {
-//       y: {
-//         ticks: {
-//           beginAtZero: true
-//         }
-//       }
-//     }
-//   }
-// });
+function displayAll(data) {
+  allDiv = document.getElementById("all");
+  const heading = document.createElement("h6");
+  heading.innerHTML = "Output from API: " + JSON.stringify(data);
+  allDiv.append(heading);
+}
 
-// const labels = [
-//   'January',
-//   'February',
-//   'March',
-//   'April',
-//   'May',
-//   'June',
-// ];
+function displayPlot(data) {
+  const labels = data.map(({ date }) => date);
+  const ydata = data.map(({ mean_bpm }) => mean_bpm)
+  const heartData = {
+    labels: labels,
+    datasets: [{
+      label: 'Heart Rate Data',
+      backgroundColor: 'rgb(0, 145, 255, 0.459)',
+      borderColor: 'rgb(0, 145, 255)',
+      data: ydata,
+      tension: 0.1,
+    }]
+  };
+  
+  const config = {
+    type: 'line',
+    data: heartData,
+    options: {}
+  };
+  
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
+}
 
-// const data = {
-//   labels: labels,
-//   datasets: [{
-//     label: 'My First dataset',
-//     backgroundColor: 'rgb(255, 99, 132)',
-//     borderColor: 'rgb(255, 99, 132)',
-//     data: [0, 10, 5, 2, 20, 30, 45],
-//     tension: 0.1,
-//   }]
-// };
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+];
 
-// const config = {
-//   type: 'line',
-//   data: data,
-//   options: {}
-// };
+// let heartData
+// let labels = data.map(({ key }) => key);
 
-// const myChart = new Chart(
-//   document.getElementById('myChart'),
-//   config
-// );
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'Heart Rate Data',
+    backgroundColor: 'rgb(0, 145, 255, 0.459)',
+    borderColor: 'rgb(0, 145, 255)',
+    data: [0, 10, 5, 2, 20, 30, 45],
+    tension: 0.5,
+  }]
+};
+
+const config = {
+  type: 'line',
+  data: data,
+  options: {}
+};
+
+const myChart = new Chart(
+  document.getElementById('myChart'),
+  config
+);
